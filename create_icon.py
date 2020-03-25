a6d5e511b9575bcc3e0e8fc3b34c80331fe391d1
Wanchen He <hwc14@qq.com>
2020-03-25 22:53:10 +0800

    release

--- create_icon.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-
-with open('xiguapig.ico', 'rb') as icon:
-    b64str = base64.b64encode(icon.read())
-
-with open('xiguapig.py', 'w') as f:
-    f.write('img = \'{}\''.format(b64str))
